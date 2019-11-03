f4cc2c7dab3676adc49c16cdba5681b3c3180133
Abinavraj5427 <abinavraj10@gmail.com>
2019-11-02 23:55:27 -0500

    Created Login class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
-export default class Login extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username:'', password:'' };
   }
   
   render(){
     return(
-      <div>
-        <h1>Login</h1>
-        <br/>
-        <input placeholder = "Username" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
-        <br/>
-        <input placeholder = "Password" value={this.state.password} onChange={event => this.setState({password: event.target.value})}/>
-        <br/>
-        <p>{this.state.username}</p>
-        <p>{this.state.password}</p>
-        <input type = "submit"/>
-      </div>
+      <Login/>
     );
   }
 }
