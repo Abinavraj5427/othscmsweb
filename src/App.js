b78fc95a58a63f940a6fa75b2d3601735c56b66a
Abinavraj5427 <abinavraj10@gmail.com>
2019-11-02 23:02:57 -0500

    Created a basic login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <p> Hello World </p>
-  );
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username:'', password:'' };
+  }
+  
+  render(){
+    return(
+      <div>
+        <h1>Login</h1>
+        <br/>
+        <input placeholder = "Username" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
+        <br/>
+        <input placeholder = "Password" value={this.state.password} onChange={event => this.setState({password: event.target.value})}/>
+        <br/>
+        <p>{this.state.username}</p>
+        <p>{this.state.password}</p>
+        <input type = "submit"/>
+      </div>
+    );
+  }
 }
 
-export default App;
