3dfc511ae1eddafb9d12381d9242a64385e03588
Sai Juttu <saijuttu30@gmail.com>
2019-11-11 07:44:29 -0600

    first styling attempt login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import './App.css';
 import Login from './components/Login';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render(){
     return(
       <Login/>
+      
     );
   }
 }
-
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,29 @@
 import React from 'react';
-
+import './Login.css'
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = { username:'', password:'' };
   }
-  
+
   render(){
+
     return(
-      <div>
-        <h1>Login</h1>
-        <br/>
-        <input placeholder = "Username" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
-        <br/>
-        <input placeholder = "Password" value={this.state.password} onChange={event => this.setState({password: event.target.value})}/>
-        <br/>
-        <p>{this.state.username}</p>
-        <p>{this.state.password}</p>
-        <input type = "submit"/>
+
+      <div class="blurred-box">
+        <div>
+          <h1>Login</h1>
+          <br/>
+          <input placeholder = "Username" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
+          <br/>
+          <input placeholder = "Password" value={this.state.password} onChange={event => this.setState({password: event.target.value})}/>
+          <br/>
+          <p>{this.state.username}</p>
+          <p>{this.state.password}</p>
+          <input type = "submit"/>
+        </div>
       </div>
+
     );
   }
 }
-
