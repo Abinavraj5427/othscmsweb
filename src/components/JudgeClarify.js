126659ad500000c045322f82b804b3be5479e5d7
Abinavraj5427 <abinavraj10@gmail.com>
2019-12-03 23:27:24 -0600

    Fixed clarifications

--- src/components/JudgeClarify.js
+++ src/components/JudgeClarify.js
@@ -16,19 +16,36 @@ export default class JudgeClarify extends React.Component
         user: "",
         newQuestion: "",
         clarID: undefined,
+        message: "",
     }
     this.getQuestions = this.getQuestions.bind(this);
     this.setUser = this.setUser.bind(this);
+    this.updateAnswer = this.updateAnswer.bind(this);
   }
 
   componentDidMount(){
       this.setUser();
       this.getQuestions();
+
+  }
+
+  updateAnswer(){
+    axios.post('http://localhost/othscmsbackend/answer_clarification.php',{
+        id: this.state.clarID,
+        answer: this.state.answer,
+    })
+    .then(result => {
+      console.log(result);
+      this.getQuestions();
+      this.setState({message: "Successfully answered question "+this.state.clarID, answer: ""});
+    })
+    .catch(error => console.log(error));
   }
 
   getQuestions(){
     axios.post('http://localhost/othscmsbackend/get_clarifications.php',{})
     .then(result => {
+      !this.state.clarID && this.setState({clarID: result.data[0].id});
       this.setState({clarifications: result.data});
     })
     .catch(error => console.log(error));
@@ -57,10 +74,10 @@ export default class JudgeClarify extends React.Component
             <div>
                 <h1>Clarifications</h1>
                 <hr/>
-               
+                
                 <h2>Answer Questions</h2>
 
-                <b>Problem ID: </b>
+                <b>Question ID: </b>
                 <select onChange  = {(e) => this.setState({clarID: e.target.value})}>
                   {
                     this.state.clarifications.length >= 1 && this.state.clarifications.map(clar => 
@@ -69,12 +86,15 @@ export default class JudgeClarify extends React.Component
                   }
                 </select>
 
-                <input type = "text" placeholder = "Answer"/>
-                <input type = "submit" onClick = {() =>{}}/>
+                <input type = "text" placeholder = "answer" value = {this.state.answer} onChange={event => this.setState({answer: event.target.value})}/>
+                <input type = "submit" onClick = {() =>{this.updateAnswer()}}/>
+                
+                <h4>{this.state.message}</h4>
+
                 <h2>Recently Asked</h2>
                 <table>
                     <tr>
-                        <th>Problem ID</th>
+                        <th>Question ID</th>
                         <th>Team</th>
                         <th>Question</th>
                         <th>Answer</th>
