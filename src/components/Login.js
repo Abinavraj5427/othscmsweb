8c2b0df3b6bf25baa55424181e7cf0332b1a600d
Abinavraj5427 <abinavraj10@gmail.com>
2019-12-05 08:01:40 -0600

    Added enter button feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,7 @@ export default class Login extends React.Component {
     super(props);
     this.state = { username:'', password:'', response: '', authenticated:false};
     this.handleClick = this.handleClick.bind(this);
+    this.enterPressed = this.enterPressed.bind(this);
   }
 
   componentDidMount(){
@@ -33,6 +34,25 @@ export default class Login extends React.Component {
             .catch(error => console.log(error));
   }
 
+  enterPressed(event){
+    var code = event.keyCode || event.which;
+    if(code === 13) { //13 is the enter keycode
+    axios.post('http://localhost/othscmsbackend/login.php',
+            {
+              username: this.state.username,
+              password: this.state.password,
+            })
+            .then(result => {
+              console.log(result);
+              cookie.save('auth-token', result.data.auth_key);
+              this.props.login();
+              this.props.history.push('/home');
+            })
+            .catch(error => console.log(error));
+                    //Do stuff in here
+    }
+  }
+
   render(){
     return(
       <div class = "login-styling">
@@ -42,11 +62,11 @@ export default class Login extends React.Component {
             <div style = {loginstyles}>
               <h1>Login</h1>
               <br/>
-              <input class="username" placeholder = "Username" style = {{margin: 10}} type = 'text' value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
+              <input class="username" placeholder = "Username" style = {{margin: 10}} type = 'text' value={this.state.username} onChange={event => this.setState({username: event.target.value})} onKeyPress={event => this.enterPressed(event)}/>
               <br/>
-              <input class="password" placeholder = "Password" style = {{margin: 10}} type = 'password' value={this.state.password} onChange={event => this.setState({password: event.target.value})}/>
+              <input class="password" placeholder = "Password" style = {{margin: 10}} type = 'password' value={this.state.password} onChange={event => this.setState({password: event.target.value})} onKeyPress={event => this.enterPressed(event)}/>
               <br/>
-              <input class="button" type="image" src={require('./entericon.jpg')} onClick={this.handleClick} height="25" width="25"/>
+              <input class="button" type="image" onKeyPress={event => this.enterPressed(event)} src={require('./entericon.jpg')} onClick={this.handleClick} height="25" width="25"/>
 
 
             </div>
