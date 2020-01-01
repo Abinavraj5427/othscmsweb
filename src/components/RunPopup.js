07dab6c25818e48d52b6604cb379983ad618c9f9
Abinavraj5427 <abinavraj10@gmail.com>
2020-01-01 16:23:27 -0600

    Changed styling

--- src/components/RunPopup.js
+++ src/components/RunPopup.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import "../styles/popup.css";
+import {ip} from "../network";
+const axios = require('axios');
 
 export default class RunPopup extends React.Component {
     constructor(props){
       super(props);
-
+      this.state = {
+        filePath: '',
+        systemTime: '',
+        team: '',
+      }
+      this.collectProblemData = this.collectProblemData.bind(this);
     }
+
+    componentDidMount(){
+      this.collectProblemData();
+    }
+
+    collectProblemData(){
+      axios.post("http://"+ip+'/othscmsbackend/problemdata.php', 
+      {
+        id: this.props.id,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        }
+      }).then(result=>{
+        this.setState(
+          {
+            filePath: result.data.filePath,
+            systemTime: result.data.systemTime,
+            team: result.data.team,
+          });
+        console.log(result)
+      }).catch(error => console.log(error));
+    }
+
     render() {
       return (
         <div className='popup'>
           <div className='popup_inner'>
-            <h1>{this.props.id}</h1>
+            <h2>{this.state.filePath}</h2>
+            <h2>{this.state.systemTime}</h2>
+            <h2>{this.state.team}</h2>
             <button onClick={this.props.closePopup}>close me</button>
           </div>
         </div>
