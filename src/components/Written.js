dbba6d268ea910ba4efbcea209a6bc91777ca862
Abinavraj5427 <abinavraj10@gmail.com>
2020-01-22 23:29:38 -0600

    Update scores possible

--- src/components/Written.js
+++ src/components/Written.js
@@ -10,9 +10,12 @@ export default class Written extends React.Component {
     this.state = { 
         score: 0,
         newScore: '',
+        name: '',
+        curTeam: '',
         matchingUsers: [],
         };
     this.pullData = this.pullData.bind(this);
+    this.updateScore = this.updateScore.bind(this);
   }
  
   componentDidMount(){
@@ -22,18 +25,37 @@ export default class Written extends React.Component {
   pullData(){
     axios.post("http://"+ip+'/othscmsbackend/match_users.php', {
         name: this.state.name,
+        team: this.state.curTeam,
     },
     {
       headers: {
         "Access-Control-Allow-Origin": "*",
       }
     }).then(result=>{
+      console.log(result);
       this.setState({matchingUsers:result.data});
+    }).catch(error => console.log(error));
+  }
+
+  updateScore(member, id, score){
+    axios.post("http://"+ip+'/othscmsbackend/update_score.php', {
+        member: member,
+        id: id,
+        score: score,
+    },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      }
+    }).then(result=>{
+      this.setState({newScore: ''});
+      this.pullData();
     }).catch(error => console.log(error));
   }
 
   render(){
     let list = this.state.matchingUsers;
+
     return(
       <div>
       <Navigation />
@@ -41,6 +63,9 @@ export default class Written extends React.Component {
           <h1>Written Score Input</h1>
 
           <input type = "text" placeholder = "search name" value = {this.state.name} onChange = {event => this.setState({name: event.target.value})}/>
+          <input type =  "submit" onClick = {() => {this.pullData()}}/>
+          <br/>
+          <input type = "text" placeholder = "search team" value = {this.state.curTeam} onChange = {event => this.setState({curTeam: event.target.value})}/>
           <input type =  "submit" onClick = {() => {this.pullData()}}/>
 
 
@@ -62,7 +87,7 @@ export default class Written extends React.Component {
                       <td>{user.school}</td>
                       <td>{user.score}</td>
                       <td><input type = "number" placeholder = "score" value = {this.state.newScore} onChange = {event => this.setState({newScore: event.target.value})}/></td>
-                      <td><input type = 'submit' onClick = {() => {}} /></td>
+                      <td><input type = 'submit' onClick = {() => {this.updateScore(user.member, user.teamid, this.state.newScore)}} /></td>
                     </tr>
                   )
                 }
